a958e83a6bf2f09d35bb26bd680a55614e350a48
nikonst <nikonst@otenet.gr>
2025-01-05 12:13:21 +0200

    Fix reading the json file

--- restAPI/main.go
+++ restAPI/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
 
@@ -24,19 +26,29 @@ func getBooks(context *gin.Context) {
 func main() {
 	router := gin.Default()
 
-	dataFile, err := os.Open("data/books.json")
+	// Open the JSON file
+	jsonFile, err := os.Open("data/books.json")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-	var response map[string][]Book
-	err = json.NewDecoder(dataFile).Decode(&response)
+	defer jsonFile.Close()
+
+	// Read the JSON file
+	byteValue, err := io.ReadAll(jsonFile)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
-	defer dataFile.Close()
+	// Unmarshal the JSON data into a slice of Person structs
+	var books []Book
+	if err := json.Unmarshal(byteValue, &books); err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the data
+	for _, book := range books {
+		fmt.Printf("Name: %s, Age: %s, Email: %s\n", book.Id, book.Title, book.Author)
+	}
 
 	router.GET("/books", getBooks)
 	router.Run("localhost:9090")
